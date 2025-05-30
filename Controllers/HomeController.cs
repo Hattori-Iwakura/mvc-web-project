28806058185d1d021911b74483c200ddd79f5e1b
Nsm <nhatnsm1225@gmail.com>
2025-05-30 23:17:00 +0700

    [added] login css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc_web_project.Data;
 using mvc_web_project.Models;
 
 namespace mvc_web_project.Controllers;
@@ -7,15 +9,23 @@ namespace mvc_web_project.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly FitnessDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, FitnessDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var products = await _context.Products
+            .Include(p => p.Category)
+            .OrderByDescending(p => p.CreatedAt)
+            .Take(8)  // Lấy 8 sản phẩm mới nhất
+            .ToListAsync();
+
+        return View(products);
     }
 
     public IActionResult Privacy()
--- obj/Debug/net8.0/mvc-web-project.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc-web-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc-web-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1a4890e099d918991b78983b45662ee81633b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425b3949d8e1c07ecb5ff4ba5436a42092db7265")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc-web-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc-web-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
